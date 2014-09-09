170a70db7479047dad99ee944d10239a11868355
pankaj <pankaj.jha@staples.com>
2014-09-09 11:35:33 -0400

    format changes in plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,32 +1,32 @@
 #
 
-define jenkins::plugin 
+define jenkins::plugin
 (
 
-	$name		 ,
-	$plugin         = "${name}.hpi",
-	$version	= 'latest',            
-	$plugin_dir     = '/var/lib/jenkins/plugins',
-	
+  $name     ,
+  $plugin     = "${name}.hpi",
+  $version    = 'latest',
+  $plugin_dir = '/var/lib/jenkins/plugins',
+  
 )
 
-{	
-	if ($version == 'latest' ){
-		$url="http://updates.jenkins-ci.org/latest/${plugin}"
-	}
-	 else {
-		$url="http://updates.jenkins-ci.org/download/plugins/${name}/${version}/${plugin}"
-	}
-	
-	 exec { "download-${name}" :
-     	 command => "wget --no-check-certificate ${url}",
-         unless	 => "test -d ${plugin_dir}/${name}",
-     	 cwd     => $plugin_dir,
-         path    => ['/usr/bin', '/usr/sbin', '/bin'],
-	 logoutput => true,
-	 notify	 => Service['jenkins'],	
-   	 }
-	
+{
+  if ($version == 'latest' ){
+    $url="http://updates.jenkins-ci.org/latest/${plugin}"
+  }
+   else {
+    $url="http://updates.jenkins-ci.org/download/plugins/${name}/${version}/${plugin}"
+  }
+  
+    exec { "download-${name}" :
+      command       => "wget --no-check-certificate ${url}",
+      unless        => "test -d ${plugin_dir}/${name}",
+      cwd           => $plugin_dir,
+      path          => ['/usr/bin', '/usr/sbin', '/bin'],
+      logoutput     => true,
+      notify        => Service['jenkins'],
+      }
+  
 
 }
 
